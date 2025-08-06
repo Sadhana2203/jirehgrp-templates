0a63fa6181a32da3a73381b0a06b77bfa146b5f1
baslieal07 <bazzdave07@gmail.com>
2025-08-06 15:59:48 +0300

    templates update nextjs, react, svelte, and vue

--- template/nextjs/src/hooks/useTranslation.ts
+++
@@ -1,9 +0,0 @@
-// @/hooks/useTranslation.ts
-
-import { useLanguage } from "@/components/context/LanguageContext";
-import { translations } from "@/translations";
-
-export function useTranslation() {
-  const { language } = useLanguage();
-  return translations[language];
-}
